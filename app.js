818124da4502a886d5f5c09afa9184cace2d50ac
alexsh882 <alexsh882@gmail.com>
2023-08-09 12:29:51 -0300

    configuración de app.js con paqueres, plantillas y listeners

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
- 
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const path = require("path");
+
+//variables de entorno
+dotenv.config({ path: ".env" });
+
+const app = express();
+
+//configuración del motor de plantillas
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+//Carpeta public para archivos estaticos
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+app.use(morgan("dev"));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor en ${process.env.APP_URL}:${process.env.PORT}`);
+});
